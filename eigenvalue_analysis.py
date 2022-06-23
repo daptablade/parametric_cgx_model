7382f5cd8cf13bcc9c1b95c14da1098c19f0cc1c
Olivia Stodieck <55671906+ostodieck@users.noreply.github.com>
2022-06-23 16:23:07 +0100

    implement sparce matrices and eigenvalue verification tests

--- eigenvalue_analysis.py
+++ eigenvalue_analysis.py
@@ -1,10 +1,12 @@
-from scipy.sparse import coo_matrix, bsr_matrix, dok_matrix
+from scipy.sparse import csr_matrix
 from scipy.sparse.linalg import eigsh, eigs
 from scipy.linalg import eig, eigh
 import numpy as np
 
 from pathlib import Path
 import csv
+
+from utils import timeit
 
 
 def read_matrix_csv(file, delimiter=" ", skip_lines_with=None):
@@ -23,6 +25,7 @@ def read_matrix_csv(file, delimiter=" ", skip_lines_with=None):
     return np.array(data)
 
 
+@timeit
 def get_matrix(data, filter_out_rows=None):
 
     mask = np.ones(len(data), dtype=bool)
@@ -48,7 +51,7 @@ def get_matrix(data, filter_out_rows=None):
         matrix[int(row[0] - 1), int(row[1] - 1)] = row[2]
     submatrix = np.where(np.any(matrix < 0, axis=1) | np.any(matrix > 0, axis=1))[0]
     mat = matrix[np.reshape(submatrix, (-1, 1)), submatrix]
-    return mat + np.tril(mat.T, k=-1)
+    return csr_matrix(mat + np.tril(mat.T, k=-1))
 
 
 # def node_dir_2_index(node_dir, dof_per_node=3):
@@ -64,9 +67,27 @@ def get_rows_from_dofs(dofs, lookup):
     return rows
 
 
-def main(
-    file="ccx_normal_modes_matout", folder="outputs/test_mat_out/test_simple_plate"
-):
+@timeit
+def get_normal_modes(problem):
+    # solve an eigenvalue problem to obtain 10 lowest eigenvalues
+    # evals_small, evecs_small = eigs(
+    #     problem["sti"], k=10, M=problem["mas"], sigma=0.0, which="LR"
+    # )
+    # # print frequencies in Hz
+    # omega = np.sqrt(evals_small) / (2 * np.pi)
+    # print("Eigenvalues (Hz): ", omega)
+
+    evals_small, evecs_small = eigsh(
+        problem["sti"], k=10, M=problem["mas"], sigma=0.0, which="LM"
+    )
+    # print frequencies in Hz
+    omega = np.sqrt(evals_small) / (2 * np.pi)
+    print("Eigenvalues (Hz): ", omega)
+
+    return omega, evals_small, evals_small
+
+
+def main(file, folder):
 
     # dofs that are constrained
     csv_file = Path(folder, "SPC_123456.bou")
@@ -86,29 +107,53 @@ def main(
         problem[matrix] = get_matrix(data)  # , filter_out_rows=fixed_dofs_rows)
 
     # check that the stiffness matrix is positive definite
-    if not all(eig(problem["sti"])[0] > 0):
+    if not all(eigsh(problem["sti"])[0] > 0):
         print("Warning: Stiffness matrix is not positive definite.")
-    if not all(eig(problem["mas"])[0] > 0):
+    if not all(eigsh(problem["mas"])[0] > 0):
         print("Warning: Mass matrix is not positive definite.")
 
-    # solve an eigenvalue problem to obtain 10 lowest eigenvalues
-    evals_small, evecs_small = eigs(
-        problem["sti"], k=10, M=problem["mas"], sigma=0.0, which="LR"
-    )
-    # print frequencies in Hz
-    omega = np.sqrt(evals_small) / (2 * np.pi)
-    print("Eigenvalues (Hz): ", omega)
+    omega, evals_small, evals_small = get_normal_modes(problem)
 
-    evals_small, evecs_small = eigsh(
-        problem["sti"], k=10, M=problem["mas"], sigma=0.0, which="LM"
-    )
-    # print frequencies in Hz
-    omega = np.sqrt(evals_small) / (2 * np.pi)
-    print("Eigenvalues (Hz): ", omega)
+    return omega
 
     # results = eig(problem["sti"], b=problem["mas"])
     # print(np.sqrt(np.sort(results[0])) / (2 * np.pi))
 
 
 if __name__ == "__main__":
-    main()
+
+    freq_scipy = main(
+        file="ccx_normal_modes_matout", folder="outputs/test_mat_out/test_simple_plate"
+    )
+    freq_ccx = np.array(
+        [
+            0.1112296e02,
+            0.3969891e02,
+            0.7415313e02,
+            0.1408884e03,
+            0.3089473e03,
+            0.4026458e03,
+            0.7748749e03,
+            0.2052274e04,
+            0.2144465e04,
+            0.3231235e04,
+        ]
+    )
+    assert np.allclose(freq_scipy, freq_ccx, rtol=1e-3)
+
+    freq_scipy = main(file="ccx_normal_modes_matout", folder="outputs/test_mat_out")
+    freq_ccx = np.array(
+        [
+            0.8692880e01,
+            0.4375812e02,
+            0.5737775e02,
+            0.6076665e02,
+            0.9949578e02,
+            0.1538641e03,
+            0.1741874e03,
+            0.2012603e03,
+            0.2374603e03,
+            0.2688630e03,
+        ]
+    )
+    assert np.allclose(freq_scipy, freq_ccx, rtol=1e-3)
